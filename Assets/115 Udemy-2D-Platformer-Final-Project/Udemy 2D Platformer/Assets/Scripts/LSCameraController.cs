b2035b0fbec4e8f1d266e00cf411e11a0091aaf3
vvan汪汪队大队长 <whu13611614482@163.com>
2025-07-10 11:12:40 +0800

    First commit

---
+++ Assets/115 Udemy-2D-Platformer-Final-Project/Udemy 2D Platformer/Assets/Scripts/LSCameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LSCameraController : MonoBehaviour
+{
+    public Vector2 minPos, maxPos;
+
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    //让相机在玩家操作的角色移动后再进行移动
+    void LateUpdate()
+    {
+        float xPos = Mathf.Clamp(target.position.x, minPos.x, maxPos.x);
+        float yPos = Mathf.Clamp(target.position.y, minPos.y, maxPos.y);
+
+        transform.position = new Vector3(xPos, yPos, transform.position.z);
+    }
+}
